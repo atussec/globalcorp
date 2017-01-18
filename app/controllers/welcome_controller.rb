077e51c2d191723b5815374cc6874ee2f81af493
Marvin Hofmann <marvin@marv-productions.de>
2017-01-18 23:23:45 +0100

    added welcome and anonymous features

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,30 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def anonymous
+    if user_signed_in?
+      redirect_to edit_user_registration_path
+    end
+    @user = generate_anonymous
+    sign_in(@user)
+  end
+
+
+  private
+  # Private: create an anonymous account for this user.
+  #
+  # email and password share the same random token
+  # to allow the user to change his password later on
+  # without it being a standard one.
+  #
+  # Warning: The users email should not be leaked!
+  #
+  # Returns the user created and saved.
+  def generate_anonymous
+    temp_token = SecureRandom.base64(15).tr('+/=', 'xyz')
+    usr = ::User.new(email: "#{temp_token}@users.globalcorp.international", password: temp_token, password_confirmation: temp_token)
+    usr.save!(validate: false)
+    usr
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Public: create an anonymous account for this user.
-  #
-  # email and password share the same random token
-  # to allow the user to change his password later on
-  # without it being a standard one.
-  #
-  # Warning: The users email should not be leaked!
-  #
-  # Returns the user created and saved.
-  def anonymous!
-    temp_token = SecureRandom.base64(15).tr('+/=', 'xyz')
-    usr = ::User.new(email: "#{temp_token}@users.globalcorp.international", password: temp_token, password_confirmation: temp_token)
-    usr.save!(validate: false)
-    usr
-  end
-
 end
