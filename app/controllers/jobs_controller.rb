d80c514589d9e12ed395f2699fdefab35cddbe62
Marvin Hofmann <marvin@marv-productions.de>
2017-01-23 10:58:53 +0100

    remove more skeleton and add redirects

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,15 +1,23 @@
 class JobsController < ApplicationController
-  before_action :set_job, only: [:show, :edit, :update, :destroy]
+  before_action :set_job, only: [:show]
 
   # GET /jobs
   # GET /jobs.json
   def index
-    @jobs = Job.all
+    # we don't want anyone to see all jobs
+    redirect_to job_boards_path
   end
 
   # GET /jobs/1
   # GET /jobs/1.json
   def show
+    # Display the job in it's board
+    redirect_to @job.job_board
+  end
+
+  # A player clicked start on a job
+  def start
+
   end
 
   private
