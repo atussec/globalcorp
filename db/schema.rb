0f297ef1734e6a5e164cc6ffe3d909f371d9b3d6
Marvin Hofmann <marvin@marv-productions.de>
2017-01-23 15:11:38 +0100

    Add skill level to jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123132610) do
+ActiveRecord::Schema.define(version: 20170123141029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(version: 20170123132610) do
     t.decimal  "money"
     t.integer  "time"
     t.integer  "job_board_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "skill",        default: 0
     t.index ["job_board_id"], name: "index_jobs_on_job_board_id", using: :btree
   end
 
@@ -37,6 +38,8 @@ ActiveRecord::Schema.define(version: 20170123132610) do
     t.integer  "user_id"
     t.decimal  "money",      precision: 999, scale: 2, default: "0.0"
     t.integer  "tutorial",                             default: 0
+    t.decimal  "skill",      precision: 10,  scale: 2, default: "0.0"
+    t.boolean  "admin",                                default: false, null: false
     t.index ["name"], name: "index_players_on_name", unique: true, using: :btree
     t.index ["user_id"], name: "index_players_on_user_id", using: :btree
   end
